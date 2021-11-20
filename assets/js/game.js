16f735bdc0e0204e2c446732711a8a597c9c5166
Nima Mehrjoo <nmehrjoo1999@gmail.com>
2021-11-20 14:54:11 -0500

    highScore will now be saved

--- assets/js/game.js
+++ assets/js/game.js
@@ -113,12 +113,20 @@ var startGame = function(){
     endGame();
 };
 var endGame = function(){
-    //if player is still alive, player wins!
-    if (playerInfo.health > 0 ){
-        window.alert("Great job, you've survived the game! You now have a score of "+ playerInfo.money + ".");
-    }else{
-        window.alert("You've lost your robot in battle. ");
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null){
+        highScore = 0;
     }
+    if (playerInfo.money > highScore){
+        localStorage.setItem("highScore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else{
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
     if (playAgainConfirm){
